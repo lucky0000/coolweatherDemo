595be35f0c2eeb3818087dddd608b7c2a602afa0
LuckY <qmr108@163.com>
2018-04-10 13:35:42 +0800

    加入数据库和表

---
+++ app/src/main/java/com/coolweather/android/db/Province.java
@@ -0,0 +1,34 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+public class Province  extends DataSupport {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    private int provinceCode;
+    private String provinceName;
+}
